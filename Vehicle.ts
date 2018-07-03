4aade116f3ce6d062e80dd2216961381dad0467c
eswaribala <Parameswaribala@gmail.com>
2018-07-03 12:43:50 +0530

    Update Vehicle.ts

--- Vehicle.ts
+++ Vehicle.ts
@@ -1,15 +1,26 @@
-export class Vehicle
+class Vehicle
 {
     private regNo:string;
     private type:string;
     private fuelType:string;
+    private dop:Date;
     constructor(regNo:string,type:string,fuelType:string)
     {
         this.regNo=regNo;
         this.type=type;
         this.fuelType=fuelType;
     }
-   
+
+    set DOP(value:Date)
+    {
+        this.dop=value;
+    }
+
+    get DOP():Date
+    {
+        return this.dop;
+    }
+
      get RegNo():string
      {
          return this.regNo;
@@ -23,3 +34,9 @@ export class Vehicle
          return this.Type;
      }
 }
+
+var obj=new Vehicle("TN-32-265427","Two Wheeler",
+    "Petrol")
+obj.DOP=new Date(2016,1,25)
+console.log(obj.RegNo)
+console.log(obj.DOP);
